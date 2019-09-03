701f0ee379df52693c949f2058d0bed5857e79a6
johnathan2724 <agusmanj9@gmail.com>
2019-09-03 15:10:15 +0700

    Auto stash before merge of "develop" and "origin/develop"

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -15,6 +15,8 @@
     <link rel="icon" href="{{ URL::to('img/core-img/favicon.ico')}}">
 
     <!-- Core Style CSS -->
+    
+    <link rel="stylesheet" href="{{ URL::to('login.css') }}">
     <link rel="stylesheet" href="{{ URL::to('css/core-style.css') }}">
     <link rel="stylesheet" href="{{ URL::to('style.css') }}">
 
