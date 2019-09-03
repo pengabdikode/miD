3e602ad919369a72f3304e61fd0adc742cff5e6d
shendy <shendyradhya@gmail.com>
2019-09-03 14:47:02 +0700

    initial commit

---
+++ resources/views/shop.blade.php
@@ -0,0 +1,167 @@
+@extends('layouts.master')
+
+@section('title','Shop')
+
+@section('content')
+    <!-- ##### Breadcumb Area Start ##### -->
+    <div class="breadcumb_area bg-img" style="background-image: url(img/bg-img/breadcumb.jpg);">
+        <div class="container h-100">
+            <div class="row h-100 align-items-center">
+                <div class="col-12">
+                    <div class="page-title text-center">
+                        <h2>katalog Kampret</h2>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- ##### Breadcumb Area End ##### -->
+
+    <!-- ##### Shop Grid Area Start ##### -->
+    <section class="shop_grid_area section-padding-80">
+        <div class="container">
+            <div class="row">
+                <div class="col-12 col-md-4 col-lg-3">
+                    <div class="shop_sidebar_area">
+
+                        <!-- ##### Single Widget ##### -->
+                        <div class="widget catagory mb-50">
+                            <!-- Widget Title -->
+                            <h6 class="widget-title mb-30">Kategori</h6>
+
+                            <!--  Catagories  -->
+                            <div class="catagories-menu">
+                                <ul>
+                                    @foreach($categories as $category)
+                                        <li><a href="{{ route('shop.index',['category' => $category->slug]) }}">{{ $category->name }}</a></li>
+                                    @endforeach
+                                </ul>
+                            </div>
+                        </div>
+
+                        <!-- ##### Single Widget ##### -->
+                        <div class="widget catagory mb-50">
+                            <!-- Widget Title -->
+                            <h6 class="widget-title mb-30">Harga</h6>
+
+                            <!--  Catagories  -->
+                            <div class="catagories-menu">
+                                <ul>
+                                    <li><a href="{{ route('shop.index',['category' => request()->category, 'sort' => 'low_high']) }}">Rendah ke Tinggi</a></li>
+                                    <li><a href="{{ route('shop.index',['category' => request()->category, 'sort' => 'high_low']) }}">Tinggi ke Rendah</a></li>
+                                </ul>
+                            </div>
+                        </div>
+
+                        <!-- ##### Single Widget ##### -->
+                        <div class="widget price mb-50">
+                            <!-- Widget Title -->
+                            <h6 class="widget-title mb-30">Sortir Berdasarkan</h6>
+                            <!-- Widget Title 2 -->
+                            <p class="widget-title2 mb-30">Harga</p>
+
+                            <div class="widget-desc">
+                                <div class="slider-range">
+                                    <div data-min="49" data-max="360" data-unit="$" class="slider-range-price ui-slider ui-slider-horizontal ui-widget ui-widget-content ui-corner-all" data-value-min="49" data-value-max="360" data-label-result="Range:">
+                                        <div class="ui-slider-range ui-widget-header ui-corner-all"></div>
+                                        <span class="ui-slider-handle ui-state-default ui-corner-all" tabindex="0"></span>
+                                        <span class="ui-slider-handle ui-state-default ui-corner-all" tabindex="0"></span>
+                                    </div>
+                                    <div class="range-price">Rentang : Rp. 100.000 - Rp. 5.000.000</div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-12 col-md-8 col-lg-9">
+                    <div class="shop_grid_product_area">
+                        <div class="row">
+                            <div class="col-12">
+                                <div class="product-topbar d-flex align-items-center justify-content-between">
+                                    <!-- Total Products -->
+                                    <div class="total-products">
+                                        <p><span>186</span> produk ditemukan</p>
+                                    </div>
+                                    <!-- Sorting -->
+                                    <div class="product-sorting d-flex">
+                                        <p>Urutkan berdasarkan:</p>
+                                        <form action="#" method="get">
+                                            <select name="select" id="sortByselect">
+                                                <option value="value">Rating Tertinggi</option>
+                                                <option value="value">Terbaru</option>
+                                                <option value="value">Harga: Rendah ke Tinggi</option>
+                                                <option value="value">Harga: Tinggi ke Rendah</option>
+                                            </select>
+                                            <input type="submit" class="d-none" value="">
+                                        </form>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="row">
+
+                        
+                            <!-- Single Product -->
+                            @forelse($products as $product)
+                            <div class="col-12 col-sm-6 col-lg-4">
+                                <div class="single-product-wrapper">
+                                    <!-- Product Image -->
+                                    <div class="product-img">
+                                        <img src="{{asset('storage/product/'.$product->foto1)}}" alt="">
+                                        <!-- Hover Thumb -->
+                                        <a href="{{route('shop.show',$product->slug)}}">
+                                        <img class="hover-img" src="{{asset('storage/product/'.$product->foto2)}}" alt="">
+                                        </a>
+                                        <!-- Product Badge -->
+                                        <div class="product-badge offer-badge">
+                                            <span>-30%</span>
+                                        </div>
+                                        <!-- Favourite -->
+                                        <div class="product-favourite">
+                                            <a href="#" class="favme fa fa-heart"></a>
+                                        </div>
+                                    </div>
+
+                                    <!-- Product Description -->
+                                    <div class="product-description text-center">
+                                        <a href="{{route('shop.show',$product->slug)}}">
+                                            <h6>{{$product->name}}</h6>
+                                        </a>
+                                        <p class="product-price"><span class="old-price">@currency($product->price)</span>@currency($product->price)</p>
+
+                                        <!-- Hover Content -->
+                                        <div class="hover-content">
+                                            <!-- Add to Cart -->
+                                            <div class="add-to-cart-btn">
+                                                <a href="#" class="btn essence-btn">Tambah Keranjang</a>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            @empty
+                            <div>Tidak ada produk ditemukan</div>
+                            @endforelse
+                            
+                        </div>
+                    </div>
+                    <!-- Pagination -->
+                    <nav aria-label="navigation">
+                        <ul class="pagination mt-50 mb-70">
+                            <li class="page-item"><a class="page-link" href="#"><i class="fa fa-angle-left"></i></a></li>
+                            <li class="page-item"><a class="page-link" href="#">1</a></li>
+                            <li class="page-item"><a class="page-link" href="#">2</a></li>
+                            <li class="page-item"><a class="page-link" href="#">3</a></li>
+                            <li class="page-item"><a class="page-link" href="#">...</a></li>
+                            <li class="page-item"><a class="page-link" href="#">21</a></li>
+                            <li class="page-item"><a class="page-link" href="#"><i class="fa fa-angle-right"></i></a></li>
+                        </ul>
+                    </nav>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- ##### Shop Grid Area End ##### -->
+@endsection
