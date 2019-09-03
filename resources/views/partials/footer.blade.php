e109bcde51d8995bc97e91fb84ffcd5e797edf35
roy <roybercos3@gmail.com>
2019-09-03 11:14:41 +0700

    ll
    llll

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,13 @@
+<!-- ##### Footer Area Start ##### -->
+<footer class="footer_area clearfix">
+            <div class="row mt-5">
+                <div class="col-md-12 text-center">
+                    <p>
+                        <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+    Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | mid.id
+    <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+                    </p>
+                </div>
+            </div>
+    </footer>
+    <!-- ##### Footer Area End ##### -->
